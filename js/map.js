30296de53d3597b9534d06a4d8f73f5954e2ba12
Eugenia Shibkova <shibkova91@gmail.com>
2017-11-27 02:22:09 +0300

    исправила замечания

--- js/map.js
+++ js/map.js
@@ -26,24 +26,24 @@ var generateOffers = function (data) {
     return array[Math.floor(Math.random() * array.length)];
   };
 
+  var getRandomInteger = function (from, to) {
+    return Math.floor(Math.random() * (to - from) + from);
+  };
+
   var makeRandomArray = function (array) {
     var arrayCopy = array.slice();
     var newLength = Math.ceil(Math.random() * arrayCopy.length);
     var newArray = [];
     for (var i = 0; i < newLength; i++) {
-      var randomElement = Math.floor(Math.random() * arrayCopy.length);
-      newArray[i] = arrayCopy[randomElement];
+      var randomElement = getRandomInteger(0, arrayCopy.length);
+      newArray.push(arrayCopy[randomElement]);
       arrayCopy.splice(randomElement, 1);
     }
     return newArray;
   };
 
-  var getRandomInteger = function (from, to) {
-    return Math.floor(Math.random() * (to - from) + from);
-  };
-
   var shuffleArrayOfTitles = function (array) {
-    var copy = array.title.slice(0);
+    var copy = array.slice(0);
     for (var i = copy.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var element = copy[i];
@@ -53,15 +53,19 @@ var generateOffers = function (data) {
     return copy;
   };
 
+  var getNumberLeadingZero = function (number) {
+    return (number < 10) ? '0' + number : number;
+  };
+
   var makeListOfOffers = function () {
     var listOfOffers = [];
-    var newArrayOfTitles = shuffleArrayOfTitles(data);
+    var newArrayOfTitles = shuffleArrayOfTitles(data.title);
     for (var i = 0; i < data.countOfOffers; i++) {
       var locationX = getRandomInteger(300, 900);
       var locationY = getRandomInteger(100, 500);
       listOfOffers[i] = {
         author: {
-          avatar: 'img/avatars/user' + '0' + (i + 1) + '.png'
+          avatar: 'img/avatars/user' + getNumberLeadingZero(i + 1) + '.png'
         },
         offer: {
           title: newArrayOfTitles[i],
@@ -86,20 +90,20 @@ var generateOffers = function (data) {
   };
 
   var makePin = function (offer) {
-    var pin = document.querySelector('.map__pin').cloneNode(true);
-    pin.classList.remove('map__pin--main');
-    pin.removeChild(pin.childNodes[3]);
+    var pin = document.querySelector('#lodge-template').content.querySelector('.map__pin').cloneNode(true);
     pin.style.left = offer.location.x;
     pin.style.top = offer.location.y;
-    pin.draggable = false;
     pin.querySelector('img').setAttribute('src', offer.author.avatar);
     return pin;
   };
 
   var pinToMap = function (listOfOffers) {
+    var fragmentPins = document.createDocumentFragment();
     for (var i = 0; i < listOfOffers.length; i++) {
       tokyoMap.appendChild(makePin(listOfOffers[i]));
+      fragmentPins.appendChild(makePin(listOfOffers[i]));
     }
+    tokyoMap.appendChild(fragmentPins);
   };
 
   var createFeatures = function (features) {
@@ -112,20 +116,23 @@ var generateOffers = function (data) {
     return fragmentFeatures;
   };
 
-  var convertToRussian = function (type) {
-    if (type === 'flat') {
-      return 'квартира';
-    } else if (type === 'house') {
-      return 'дом';
-    } else {
-      return 'бунгало';
+  var convertTypeToRussian = function (type) {
+    switch (type) {
+      case 'flat':
+        return 'квартира';
+
+      case 'house':
+        return 'дом';
+
+      default:
+        return 'бунгало';
     }
   };
 
   var tokyoMap = document.querySelector('.map');
   tokyoMap.classList.remove('map--faded');
 
-  var similarOfferTemplate = document.querySelector('template').content;
+  var similarOfferTemplate = document.querySelector('#lodge-template').content;
   var createCard = function (listOfOffers) {
     var offerElement = similarOfferTemplate.cloneNode(true);
     var offer = listOfOffers.offer;
@@ -133,18 +140,17 @@ var generateOffers = function (data) {
     var allFeatures = listOfOffers.offer.features;
     offerElement.querySelector('.map__card h3').textContent = offer.title;
     offerElement.querySelector('.map__card p:nth-of-type(1) small').textContent = offer.address;
-    offerElement.querySelector('.popup__price').textContent = offer.price + '₽' + '/ночь';
-    offerElement.querySelector('.map__card h4').textContent = convertToRussian(offer.type);
+    offerElement.querySelector('.popup__price').textContent = offer.price + '₽' + '/ночь';//
+    offerElement.querySelector('.map__card h4').textContent = convertTypeToRussian(offer.type);
     offerElement.querySelector('.map__card p:nth-of-type(3)').textContent = 'Для ' + offer.guests + ' гостей в ' + offer.rooms + ' комнатах';
     offerElement.querySelector('.map__card p:nth-of-type(4)').textContent = 'Заезд после ' + offer.checkIn + ', выезд после ' + offer.checkOut;
 
     var offerFeature = offerElement.querySelector('.popup__features');
-
-    for (var i = 1; i <= 6; i++) {
-      offerFeature.removeChild(offerFeature.childNodes[i]);
+    while (offerFeature.firstChild) {
+      offerFeature.removeChild(offerFeature.firstChild);
     }
-
     offerFeature.appendChild(createFeatures(allFeatures));
+
     offerElement.querySelector('.map__card p:nth-of-type(5)').textContent = offer.description;
     offerElement.querySelector('img').setAttribute('src', author.avatar);
     return offerElement;
@@ -153,6 +159,7 @@ var generateOffers = function (data) {
   var listOfOffers = makeListOfOffers();
   pinToMap(listOfOffers);
   tokyoMap.appendChild(createCard(listOfOffers[0]));
+
 };
 
 generateOffers(DATA);
