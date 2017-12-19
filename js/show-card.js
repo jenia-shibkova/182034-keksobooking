0ce84e8b2bb9c26ba9008893827dceba8438e004
Eugenia Shibkova <shibkova91@gmail.com>
2017-12-19 20:58:55 +0300

    вынесла модуль cards

--- js/show-card.js
+++ js/show-card.js
@@ -1,1 +1,7 @@
 'use strict';
+
+window.showCard = function (sourceElement, targetElement, sourceData, targetData, syncValuesFunc) {
+  var index = sourceData.indexOf(sourceElement.value);
+  var targetElementValue = targetData[index];
+  syncValuesFunc(targetElement, targetElementValue);
+};
